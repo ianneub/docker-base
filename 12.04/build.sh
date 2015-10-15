dc2a422d658866d42780aaf133d56444aa4a105f
Ian Neubert <ian@ianneubert.com>
2015-10-15 14:56:57 -0700

    Add Dockerize to base image.

--- 12.04/build.sh
+++ 12.04/build.sh
@@ -2,7 +2,22 @@
 
 set -e
 
+################
+# update base system
+################
 apt-get update
-apt-get install -y supervisor inetutils-ping dnsutils
 
+################
+# install base packages
+################
+apt-get install --no-install-recommends -y supervisor inetutils-ping dnsutils curl
+
+################
+# install dockerize
+################
+curl -sL https://github.com/jwilder/dockerize/releases/download/v0.0.2/dockerize-linux-amd64-v0.0.2.tar.gz | tar -C /usr/local/bin -xz
+
+################
+# clean apt
+################
 apt-get clean && rm -rf /var/lib/apt/lists/*
